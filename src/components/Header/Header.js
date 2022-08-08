8d33f6a458a8772424ce8b7af2d11286130e3fc3
Blick <dima.8m8@gmail.com>
2022-08-08 13:10:54 +0300

    feat: add header meals image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,19 @@ import React from 'react';
 import HeaderCartButton from '../UI/HeaderCartButton/HeaderCartButton';
 
 import classes from './Header.module.css';
+import mealsImg from '../../assets/jpg/meals.jpg';
 
 const Header = () => {
   return (
-    <div className={classes.header}>
-      <h1>ReactMeals</h1>
-      <HeaderCartButton />
-    </div>
+    <React.Fragment>
+      <div className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </div>
+      <div className={classes['main-image']}>
+        <img src={mealsImg}></img>
+      </div>
+    </React.Fragment>
   );
 };
 
