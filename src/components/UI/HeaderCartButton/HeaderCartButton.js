c47a1788d898fe3ea7d9aae7b6dd945eb992854b
Blick <dima.8m8@gmail.com>
2022-08-10 13:58:53 +0300

    feat: add button bump animation

--- src/components/UI/HeaderCartButton/HeaderCartButton.js
+++ src/components/UI/HeaderCartButton/HeaderCartButton.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 
 import classes from './HeaderCartButton.module.css';
 import { HiShoppingCart } from 'react-icons/hi';
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
+  ctx.amount = ctx.items.length;
   return (
     <button className={classes.button} onClick={props.onClick}>
       <HiShoppingCart className={classes.cart} />
       <span>Your Cart</span>
-      <span className={classes['goods-amount']}>0</span>
+      <span className={classes['goods-amount']}>{ctx.amount}</span>
     </button>
   );
 };
