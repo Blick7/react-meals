a81d89ea8f72d0c4cc0f786750f0521d0f1f9a02
Blick <dima.8m8@gmail.com>
2022-08-18 14:18:06 +0300

    feat: add data fetch from firebase

--- src/components/Meals/MealsCard.js
+++ src/components/Meals/MealsCard.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MealItem from './MealItem';
-import DUMMY_MEALS from '../../dummy-meals';
 import Card from '../UI/Card/Card';
 
 import classes from './MealsCard.module.css';
 
 const MealsCard = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMealsData = async () => {
+      const response = await fetch(
+        'https://react-http-67f2d-default-rtdb.firebaseio.com/meals.json'
+      );
+      const data = await response.json();
+
+      const loadedData = [];
+
+      for (let item in data) {
+        loadedData.push({
+          id: item,
+          name: data[item].name,
+          description: data[item].description,
+          price: data[item].price,
+        });
+      }
+      // console.log(loadedData);
+      setMeals(loadedData);
+      setIsLoading(false);
+    };
+
+    fetchMealsData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className={classes.meals}>
+        <Card>
+          <p className={classes.loading}>Loading...</p>
+        </Card>
+      </div>
+    );
+  }
+
   return (
     <div className={classes.meals}>
       <Card>
-        {DUMMY_MEALS.map((item) => {
+        {meals.map((item) => {
           return (
             <MealItem
               name={item.name}
